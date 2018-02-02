b141b39c4a136af220c8fc4579eb36036a8fa957
Stella Dee <stelladeecoder@gmail.com>
2018-02-01 19:48:57 -0600

    Nested text views

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -47,14 +47,29 @@ public class MainActivity extends AppCompatActivity {
     public void addOneForTeamA(View v) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
-    }
 
+    }
+    /**
+     * Increase the score for Team B by 1 point.
+     */
+    public void addOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+
+    }
     /**
      * Increase the score for Team A by 2 points.
      */
     public void addTwoForTeamA(View v) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
+    }
+    /**
+     * Increase the score for Team B by 2 points.
+     */
+    public void addTwoForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
     }
 
     /**
@@ -64,12 +79,25 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
-
+    /**
+     * Increase the score for Team B by 3 points.
+     */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+        /**
+         * Displays the given score for Team B.
+         */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
